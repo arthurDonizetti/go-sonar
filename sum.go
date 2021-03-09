9f021b02676bf3978b17a0ffb770dba27b1651a1
Arthur Donizetti <arthur.donizetti@gmail.com>
2021-03-09 02:10:45 -0300

    ci: remove unnecessary functions

--- sum.go
+++ sum.go
@@ -9,27 +9,3 @@ func main() {
 func sum(a int, b int) int {
 	return a + b
 }
-
-func sub(a int, b int) int {
-	return a - b
-}
-
-func times(a int, b int) int {
-	return a + b
-}
-
-func sumx(a int, b int) int {
-	return a + b + a
-}
-
-func sumy(a int, b int) int {
-	return a + b + b
-}
-
-func timesB(b int) int {
-	return b * b
-}
-
-func timesA(a int) int {
-	return a * a
-}
